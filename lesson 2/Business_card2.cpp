ae56fabbbac2399ab088a43e6e870f1a7679dc2b
Ваше Имя <antonshevando@gmail.com>
2025-11-23 16:14:45 +0300

    Add homework for lesson 2

---
+++ lesson 2/Business_card2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string name = "Дмитроченко Михаил Александрович";
+	cout << "Меня зовут " << name << endl;
+	int age = 13;
+	cout << "Мне " << age << endl;
+	float height = 175;
+	cout << "Мой рост " << height << endl;
+	double weight = 78.3;
+	cout << "Я вешу " << weight << endl;
+	string color = "голубенький";
+	cout << "Мой любимый цвет " << color << endl;
+	int num = 67;
+	cout << "Моё любимое число " << num << endl;
+	return 0;
+}
---
+++ lesson 2/Currency.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num = 1;
+	cout << "В " << num << " долларе " << num * 79.02 << " рублей" << endl;
+	cout << "В " << num << " рубле " << num / 79.02 << " долларов" << endl;
+	cout << "В " << num << " евро " << num * 90.56 << " рублей" << endl;
+	cout << "В " << num << " рубле " << num / 90.56 << " евро" << endl;
+}
